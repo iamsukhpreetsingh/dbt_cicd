f9416c99df962c3f0b2c8042e9bdd70c39284444
Sukh <sukhpreetsingh@gmail.com>
2026-08-06 11:12:05 +0530

    demo change

--- models/macro_demos/demo_15_partitioning.sql
+++ models/macro_demos/demo_15_partitioning.sql
@@ -6,7 +6,7 @@
 ) }}
 
 {#
-  Snowflake: CLUSTER BY (visit_date, ticket_type) applied via post-hook.
+  Snowflake: CLUSTER BY (visit_date, ticket_type) applied via post-hook
   Override cluster columns at runtime:
     dbt run --vars '{"cluster_columns": ["visit_date", "customer_id"]}' --select demo_15_partitioning
   BigQuery: swap get_cluster_by_sql for a partition_by config key (see macro docs).
